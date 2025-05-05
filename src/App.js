f75dda7830c6b7fabea59a0b62f5406852a07961
soultef <soul_tefera@yahoo.com>
2025-05-05 10:43:57 -0500

    feat: integrate React Router for route management in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import Login from './components/layout/Login'; 
 import NavBar from './components/layout/navBar';
-// import NavBars from './navigationBar.jsx'; 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/layout/Register'; 
+import ForgotPassword from './components/layout/forgot-password';
+import Dashboard from './components/layout/Dashboard';
+import Invoices from './components/layout/Invoices';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    
-    </div>
+    <Router>
+       <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
