4ce36eed00025c236dbccfd2f77adcd1c02b09fe
soultef <soul_tefera@yahoo.com>
2025-05-05 10:51:35 -0500

    feat(Dashboard): added Dashboard UI.

---
+++ src/components/layout/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>This is Dashboard page. It comes very soon!</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
