a66c73f3e81a8c1b072c2e5e860eaeb98b309e41
soultef <soul_tefera@yahoo.com>
2025-05-05 10:54:08 -0500

    feat(forgotpassword): added forgotpassword UI.

---
+++ src/components/layout/forgot-password.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h1>This is forgot password page. It comes very soon!</h1>
+    </div>
+  )
+}
+
+export default ForgotPassword;
